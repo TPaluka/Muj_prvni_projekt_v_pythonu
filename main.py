c593b82eb47948a6fa6166db70b116b9fcf40e3d
ShuZake <shuzake2@email.cz>
2025-02-04 15:05:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+print("hello world", 15 ) #pokud chci více datových typů v printu, musím si je oddělít ","
+
+#datový typ int do které ukládám nějakou hodnotu
+cislo = 3
+
+print("Druhý způsob s proměnnou", cislo)
+
+#datový typ string do které ho ukládám text
+text = "mám velkej hlad"
+
+#do konzole vypíše oba dva datové typy
+print(text, cislo)
+
+#vytvoření vstupní hodnoty, uživatel zadává hodnotu
+#následně se hodnota zadaná do terminálu uloží do proměnné
+# input() - příkaz pro vstupní data
+vstupni_promenna = input()
+druha_vstupni_promenna = input("Zadejte číslo")
+
+#print() nám vypíše do konzole, co uživatel zadal do inputu
+print(vstupni_promenna, druha_vstupni_promenna)
+
+
+
