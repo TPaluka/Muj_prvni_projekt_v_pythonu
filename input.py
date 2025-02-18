9727e979b778968904d89b7eba6d76739ea8f0c2
ShuZake <shuzake2@email.cz>
2025-02-18 15:03:19 +0100

    Třetí

---
+++ input.py
@@ -0,0 +1,35 @@
+sklenice1 = "voda"
+sklenice2 = "šťáva"
+
+print("Sklenice1 =", sklenice1)
+print("Sklenice2 =", sklenice2)
+
+print("Přidáme sklenici3")
+
+sklenice3 = ""
+
+print ("Sklenice3 =", sklenice3)
+
+print ("Právě jsem přelil sklenici1 do sklenice3")
+sklenice3 = sklenice1
+sklenice1 = ""
+
+print("Sklenice1 =", sklenice1)
+print("Sklenice2 =", sklenice2)
+print("Sklenice3 =", sklenice3)
+
+print ("Právě jsem přelil sklenici2 do sklenice1")
+sklenice1 = sklenice2
+sklenice2 = ""
+
+print("Sklenice1 =", sklenice1)
+print("Sklenice2 =", sklenice2)
+print("Sklenice3 =", sklenice3)
+
+print ("Právě jsem přelil sklenice3 do sklenice2")
+sklenice2 = sklenice3
+sklenice3 = ""
+
+print("Sklenice1 =", sklenice1)
+print("Sklenice2 =", sklenice2)
+print("Sklenice3 =", sklenice3)
---
+++ sklenice_input.py
@@ -0,0 +1,36 @@
+sklenice1 = input("Zadejte název nápoje = ")
+sklenice2 = input("Zadejte název nápoje = ")
+
+print (f"sklenice 1 = {sklenice1}")
+print (f"sklenice 2 = {sklenice2}")
+
+print (f"Přidáme sklenici 3")
+
+sklenice3 = ""
+print (f"sklenice 1 = {sklenice1}")
+print (f"sklenice 2 = {sklenice2}")
+print (f"sklenice 3 = {sklenice3}")
+
+print ("Přelejeme sklenici 1 do sklenice 3")
+sklenice3 = sklenice1
+sklenice1 = ""
+
+print (f"sklenice 1 = {sklenice1}")
+print (f"sklenice 2 = {sklenice2}")
+print (f"sklenice 3 = {sklenice3}")
+
+print ("Přelejeme sklenici 2 do sklenice 1")
+sklenice1 = sklenice2
+sklenice2 = ""
+
+print (f"sklenice 1 = {sklenice1}")
+print (f"sklenice 2 = {sklenice2}")
+print (f"sklenice 3 = {sklenice3}")
+
+print ("Přelejeme sklenici 3 do sklenice 2")
+sklenice2 = sklenice3
+sklenice3 = ""
+
+print (f"sklenice 1 = {sklenice1}")
+print (f"sklenice 2 = {sklenice2}")
+print (f"sklenice 3 = {sklenice3}")
